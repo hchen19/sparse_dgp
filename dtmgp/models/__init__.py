aadcce874e5f3e5f1be6282390db13644f8dafed
warrenzha <wyzhao@tamu.edu>
2024-04-11 13:41:50 -0500

    enable subset training

--- dtmgp/models/__init__.py
+++ dtmgp/models/__init__.py
@@ -1,5 +1,5 @@
-from .mnist_dgp_sg import DAMGPmnist
-from .mnist_dgp_add import DTMGPmnist
+from .mnist_dgp_add_variational import DAMGPmnist
+from .mnist_dgp_sg_variational import DTMGPmnist
 from .simple_cnn_variational import SCNN
 from .simple_dgp_add_variational import SDTMGPadd
 from .simple_dgp_sg_variational import SDTMGPsg
