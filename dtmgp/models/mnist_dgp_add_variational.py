cae1cc0b35d925c331e8c78872e8f6caf6dff34e
warrenzha <wyzhao@tamu.edu>
2024-04-11 16:33:03 -0500

    fine tune model

--- dtmgp/models/mnist_dgp_add_variational.py
+++ dtmgp/models/mnist_dgp_add_variational.py
@@ -23,9 +23,10 @@ class DAMGPmnist(nn.Module):
 
         self.activation = activation
 
+        w0 = 64
         self.fc0 = LinearReparameterization(
             in_features=input_dim,
-            out_features=100,
+            out_features=w0,
             prior_mean=prior_mu,
             prior_variance=prior_sigma,
             posterior_mu_init=posterior_mu_init,
@@ -37,7 +38,7 @@ class DAMGPmnist(nn.Module):
         ## 1st layer of DGP: input:[n, input_dim] size tensor, output:[n, w1] size tensor
         #################################################################################
         # return [n, m1] size tensor for [n, input_dim] size input and [m1, input_dim] size sparse grid
-        self.tmk1 = AMGP(in_features=100, n_level=4, design_class=design_class, kernel=kernel)
+        self.tmk1 = AMGP(in_features=w0, n_level=5, design_class=design_class, kernel=kernel)
         m1 = self.tmk1.out_features # m1 = input_dim*(2^n_level-1)
         w1 = 64
         # return [n, w1] size tensor for [n, m1] size input and [m1, w1] size weights
@@ -55,7 +56,7 @@ class DAMGPmnist(nn.Module):
         ## 2nd layer of DGP: input:[n, w1] size tensor, output:[n, w2] size tensor
         #################################################################################
         # return [n, m2] size tensor for [n, w1] size input and [m2, w1] size sparse grid
-        self.tmk2 = AMGP(in_features=w1, n_level=4, design_class=design_class, kernel=kernel)
+        self.tmk2 = AMGP(in_features=w1, n_level=5, design_class=design_class, kernel=kernel)
         m2 = self.tmk2.out_features # m2 = w1*(2^n_level-1)
         w2 = output_dim
         # return [n, w2] size tensor for [n, m2] size input and [m2, w2] size weights
