a8809e7b6660b30e2e735b31a054738d1563928f
hchen19 <chenhaoyuan2018@gmail.com>
2024-05-14 16:48:31 -0500

    update extensrions

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,6 @@ extensions = [
     'sphinx_rtd_theme',
     "sphinx.ext.githubpages",
     "sphinx_autodoc_typehints",
-    "nbsphinx",
-    "m2r2",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
